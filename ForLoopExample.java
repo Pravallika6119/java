8c98cd76aacecc4b193ba35316e59871a51eec1c
pravallika sukhaveni <pravallidamini123@gmail.com>
2020-09-10 08:30:30 +0530

    array programs

--- ForLoopExample.java
+++
@@ -1,9 +0,0 @@
-package com.concent;
-
-public class ForLoopExample {
-    public static void main(String args[]){
-        for(int i=10;i>1;i--){
-            System.out.println("the value of i is:"+i);
-        }
-    }
-}
--- klass.java
+++
@@ -1,9 +0,0 @@
-package klasstudent;
-
-public class klass {
-	public static void main(String[] ar)
-	{
-		System.out.println("hello world");
-	}
-
-}
